66481c357ca05e94275765c8f7fdd068f6711b01
GITUSER <hudson@inboundnow.com>
2017-05-25 20:28:17 -0500

    First commit

---
+++ actions/notify-on-repeat-submissions.php
@@ -0,0 +1,320 @@
+<?php if (!defined('ABSPATH')) exit;
+
+/**
+ * Class NF_Action_Save
+ */
+if (class_exists('NF_Abstracts_Action')) {
+
+    final class NF_Actions_Codeable_Notify_On_Repeat_Submission extends NF_Abstracts_Action {
+        /**
+         * @var string
+         */
+        protected $_name = 'codeable-notify-administrator-on-repeat-email';
+
+        /**
+         * @var array
+         */
+        protected $_tags = array();
+
+        /**
+         * @var string
+         */
+        protected $_timing = 'normal';
+
+        /**
+         * @var int
+         */
+        protected $_priority = '10';
+
+        /**
+         * Constructor
+         */
+        public function __construct() {
+
+            parent::__construct();
+
+            $this->_nicename = __('Codeable Custom Action', 'codeable-amberresources');
+
+            $this->_settings['notify-on-repeat-email'] =  array(
+                    'name' => 'email_address',
+                    'type' => 'textbox',
+                    'width' => 'one-half',
+                    'label' => __('Notify Email Address', 'codeable-amberresources'),
+                    'width' => 'full',
+                    'value' => get_option('admin_email'),
+                    'group' => 'primary',
+                    'use_merge_tags' => false,
+                    'help' => __('For multiple email addresses, separate with commas ', 'codeable-amberresources')
+            );
+
+            $this->_settings['notify-on-repeat-email-subject'] =  array(
+                    'name' => 'subject',
+                    'type' => 'textbox',
+                    'width' => 'one-half',
+                    'label' => __('Subject line', 'codeable-amberresources'),
+                    'width' => 'full',
+                    'value' => __('Notice of repeat applicant.', 'codeable-amberresources'),
+                    'group' => 'primary',
+                    'use_merge_tags' => false,
+                    'help' => __('For multiple email addresses, separate with commas ', 'codeable-amberresources')
+            );
+
+            $this->_settings['notify-on-repeat-email-body'] =  array(
+                    'name' => 'email_body',
+                    'type' => 'textarea',
+                    'width' => 'one-half',
+                    'label' => __('Message Body', 'codeable-amberresources'),
+                    'width' => 'full',
+                    'value' => __('An applicant with a prior application has filled out a form.', 'codeable-amberresources'),
+                    'group' => 'primary',
+                    'use_merge_tags' => false,
+                    'help' => __('Displays above link to view all submissions.', 'codeable-amberresources')
+            );
+
+
+
+        }
+
+        /*
+        * PUBLIC METHODS
+        */
+
+        public function save($action_settings) {
+
+        }
+
+        /**
+         * Process the custom action
+         * @param  [type] $action_settings [description]
+         * @param  [type] $form_id         [description]
+         * @param  [type] $data            [description]
+         * @return [type]                  [description]
+         */
+        public function process($action_settings, $form_id, $data) {
+            global $wpdb;
+
+            //error_log($form_id);
+            //error_log(print_r($action_settings,true));
+            //error_log(print_r($data,true));
+
+            /* get email input value from form data */
+            $email = self::get_email_input($data);
+
+
+            /* check subscription meta for email address */
+            $duplicates = $wpdb->get_results(  "SELECT * FROM ". $wpdb->prefix . "postmeta WHERE meta_key LIKE '%field_%' AND meta_value = '".$email."'"  );
+
+            /* if there are no submissions that share the email address then abandon job */
+            if (!$duplicates) {
+                return $data;
+            }
+
+            /* Send admin notification */
+            //error_log(print_r($duplicates,true));
+            //error_log(print_r($action_settings,true));
+            self::send_admin_notification( $form_id, $duplicates , $action_settings , $email);
+            exit;
+            return $data;
+        }
+
+
+
+        /**
+         * Plucks value of first occurance of an email address field type
+         * @param  [type] $data [description]
+         * @return [type]       [description]
+         */
+        public static function get_email_input($data) {
+
+            foreach ($data['fields'] as $field) {
+                if ($field['type'] == 'email') {
+                    return $field['value'];
+                }
+            }
+        }
+
+        /**
+         * Send email notification about repeat applicant
+         * @param  [type] $form_id         [description]
+         * @param  [type] $email_addresses [description]
+         * @param  [type] $action_settings [description]
+         * @param  [type] $lookup_email    [description]
+         * @return [type]                  [description]
+         */
+        public static function send_admin_notification( $form_id ,  $email_addresses , $action_settings , $lookup_email) {
+            $emails = explode(',',$action_settings['email_address']);
+            $html = self::get_email_template();
+
+            /* replace tokens in template */
+            $lookup_url = admin_url('edit.php?s='.$lookup_email.'&post_status=all&post_type=nf_sub&action=-1&form_id='.$form_id.'&begin_date&end_date&paged=1&action2=-1');
+            $html = str_replace('{{email}}' , $lookup_email , $html );
+            $html = str_replace('{{submissions_url}}' , $lookup_url , $html );
+            $html = str_replace('{{message_body}}' , $action_settings['email_body'] , $html );
+
+            $headers = "From: " . get_bloginfo('name') . " <" .  get_bloginfo('admin_email') . ">\n";
+            $headers .= 'Content-type: text/html';
+
+            foreach ($emails as $email) {
+                wp_mail( trim($email) , $action_settings['subject'] , $html , $headers);
+            }
+        }
+
+        /**
+         * get email html template
+         * @return [type] [description]
+         */
+        public static function get_email_template() {
+            $html = '<!DOCTYPE html>
+                        <html>
+                          <head>
+                            <meta name="viewport" content="width=device-width">
+                            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+                            <title>Simple Transactional Email</title>
+                            <style type="text/css">
+                            /* -------------------------------------
+                                INLINED WITH https://putsmail.com/inliner
+                            ------------------------------------- */
+                            /* -------------------------------------
+                                RESPONSIVE AND MOBILE FRIENDLY STYLES
+                            ------------------------------------- */
+                            @media only screen and (max-width: 620px) {
+                              table[class=body] h1 {
+                                font-size: 28px !important;
+                                margin-bottom: 10px !important; }
+                              table[class=body] p,
+                              table[class=body] ul,
+                              table[class=body] ol,
+                              table[class=body] td,
+                              table[class=body] span,
+                              table[class=body] a {
+                                font-size: 16px !important; }
+                              table[class=body] .wrapper,
+                              table[class=body] .article {
+                                padding: 10px !important; }
+                              table[class=body] .content {
+                                padding: 0 !important; }
+                              table[class=body] .container {
+                                padding: 0 !important;
+                                width: 100% !important; }
+                              table[class=body] .main {
+                                border-left-width: 0 !important;
+                                border-radius: 0 !important;
+                                border-right-width: 0 !important; }
+                              table[class=body] .btn table {
+                                width: 100% !important; }
+                              table[class=body] .btn a {
+                                width: 100% !important; }
+                              table[class=body] .img-responsive {
+                                height: auto !important;
+                                max-width: 100% !important;
+                                width: auto !important; }}
+                            /* -------------------------------------
+                                PRESERVE THESE STYLES IN THE HEAD
+                            ------------------------------------- */
+                            @media all {
+                              .ExternalClass {
+                                width: 100%; }
+                              .ExternalClass,
+                              .ExternalClass p,
+                              .ExternalClass span,
+                              .ExternalClass font,
+                              .ExternalClass td,
+                              .ExternalClass div {
+                                line-height: 100%; }
+                              .apple-link a {
+                                color: inherit !important;
+                                font-family: inherit !important;
+                                font-size: inherit !important;
+                                font-weight: inherit !important;
+                                line-height: inherit !important;
+                                text-decoration: none !important; }
+                              .btn-primary table td:hover {
+                                background-color: #34495e !important; }
+                              .btn-primary a:hover {
+                                background-color: #34495e !important;
+                                border-color: #34495e !important; } }
+                            </style>
+                          </head>
+                          <body class="" style="background-color:#f6f6f6;font-family:sans-serif;-webkit-font-smoothing:antialiased;font-size:14px;line-height:1.4;margin:0;padding:0;-ms-text-size-adjust:100%;-webkit-text-size-adjust:100%;">
+                            <table border="0" cellpadding="0" cellspacing="0" class="body" style="border-collapse:separate;mso-table-lspace:0pt;mso-table-rspace:0pt;background-color:#f6f6f6;width:100%;">
+                              <tr>
+                                <td style="font-family:sans-serif;font-size:14px;vertical-align:top;">&nbsp;</td>
+                                <td class="container" style="font-family:sans-serif;font-size:14px;vertical-align:top;display:block;max-width:580px;padding:10px;width:580px;Margin:0 auto !important;">
+                                  <div class="content" style="box-sizing:border-box;display:block;Margin:0 auto;max-width:580px;padding:10px;">
+                                    <!-- START CENTERED WHITE CONTAINER -->
+                                    <span class="preheader" style="color:transparent;display:none;height:0;max-height:0;max-width:0;opacity:0;overflow:hidden;mso-hide:all;visibility:hidden;width:0;">This is preheader text. Some clients will show this text as a preview.</span>
+                                    <table class="main" style="border-collapse:separate;mso-table-lspace:0pt;mso-table-rspace:0pt;background:#fff;border-radius:3px;width:100%;">
+                                      <!-- START MAIN CONTENT AREA -->
+                                      <tr>
+                                        <td class="wrapper" style="font-family:sans-serif;font-size:14px;vertical-align:top;box-sizing:border-box;padding:20px;">
+                                          <table border="0" cellpadding="0" cellspacing="0" style="border-collapse:separate;mso-table-lspace:0pt;mso-table-rspace:0pt;width:100%;">
+                                            <tr>
+                                              <td style="font-family:sans-serif;font-size:14px;vertical-align:top;">
+                                                <p style="font-family:sans-serif;font-size:14px;font-weight:normal;margin:0;Margin-bottom:15px;">Hi there,</p>
+                                                <p style="font-family:sans-serif;font-size:14px;font-weight:normal;margin:0;Margin-bottom:15px;">
+                                                    {{message_body}}
+                                                </p>
+                                                <table border="0" cellpadding="0" cellspacing="0" class="btn btn-primary" style="border-collapse:separate;mso-table-lspace:0pt;mso-table-rspace:0pt;box-sizing:border-box;width:100%;">
+                                                  <tbody>
+                                                    <tr>
+                                                      <td align="left" style="font-family:sans-serif;font-size:14px;vertical-align:top;padding-bottom:15px;">
+                                                        <table border="0" cellpadding="0" cellspacing="0" style="border-collapse:separate;mso-table-lspace:0pt;mso-table-rspace:0pt;width:100%;width:auto;">
+                                                          <tbody>
+                                                            <tr>
+                                                              <td style="font-family:sans-serif;font-size:14px;vertical-align:top;background-color:#ffffff;border-radius:5px;text-align:center;background-color:#3498db;">
+                                                                  <a href="{{submissions_url}}" target="_blank" style="text-decoration:underline;background-color:#ffffff;border:solid 1px #3498db;border-radius:5px;box-sizing:border-box;color:#3498db;cursor:pointer;display:inline-block;font-size:14px;font-weight:bold;margin:0;padding:12px 25px;text-decoration:none;text-transform:capitalize;background-color:#3498db;border-color:#3498db;color:#ffffff;">
+                                                                  View all submissions for {{email}}
+                                                                  </a>
+                                                              </td>
+                                                            </tr>
+                                                          </tbody>
+                                                        </table>
+                                                      </td>
+                                                    </tr>
+                                                  </tbody>
+                                                </table>
+
+                                              </td>
+                                            </tr>
+                                          </table>
+                                        </td>
+                                      </tr>
+                                      <!-- END MAIN CONTENT AREA -->
+                                    </table>
+                                    <!-- END CENTERED WHITE CONTAINER -->
+                                  </div>
+                                </td>
+                                <td style="font-family:sans-serif;font-size:14px;vertical-align:top;">&nbsp;</td>
+                              </tr>
+                            </table>
+                          </body>
+                        </html>';
+
+            return $html;
+        }
+
+
+        /**
+         * Get Ninja Form submissions given a form id
+         * @param  [type] $form_id [description]
+         * @return [type]          [description]
+         */
+        public static function get_submissions( $form_id ) {
+            $args = array(
+                'post_type' => 'nf_sub',
+                'posts_per_page' => -1,
+                'meta_query'=> array(
+    				array(
+    					'key' => '_form_id',
+    					'value' => $form_id,
+    					'compare' => '=',
+    				)
+                )
+			);
+
+            $submissions = get_posts($args);
+
+            return $submissions;
+        }
+    }
+}
---
+++ codeable-amberresources.php
@@ -0,0 +1,134 @@
+<?php
+/*
+Plugin Name: Codeable.io Extension - Customizations involving Ninja Forms, WP Job Manager, and more.
+Plugin URI:
+Description:
+Version: 0.0.1
+Author: Hudson Atwell
+Author URI: http://www.github.com/atwellpub
+Text Domain: codeable-amberresources
+Domain Path: assets/lang
+*/
+
+if (!class_exists('Codeable_AmberResources')) {
+
+    final class Codeable_AmberResources {
+
+        /**
+         * Construct Codeable_AmberResources
+         */
+        public function __construct() {
+            self::define_constants();
+            self::includes();
+            self::load_hooks();
+        }
+
+        /*
+        * Setup plugin constants
+        *
+        */
+        private static function define_constants() {
+            define('CIO_AMBER_CURRENT_VERSION', '1.0.1' );
+            define('CIO_AMBER_URLPATH', plugins_url( '/' , __FILE__ ) );
+            define('CIO_AMBER_PATH', plugin_dir_path( __FILE__ ) );
+            define('CIO_AMBER_SLUG', 'codeable-amberresources' );
+            define('CIO_AMBER_FILE', __FILE__ );
+        }
+
+        /**
+         * Load WordPress Hooks and Filters
+         */
+        private static function load_hooks() {
+            add_filter( 'ninja_forms_render_default_value', array( __CLASS__ , 'autopopulate_hidden_values') , 10, 3 );
+
+			add_filter( 'ninja_forms_register_actions', array(__CLASS__ , 'register_actions'));
+
+        }
+
+        /**
+         * Include PHP Files
+         */
+        private static function includes() {
+
+            switch (is_admin()) :
+                case true :
+                    /* load admin only files */
+                    include_once CIO_AMBER_PATH . 'actions/notify-on-repeat-submissions.php';
+                    BREAK;
+
+                case false :
+                    /* load front-end files */
+                    include_once CIO_AMBER_PATH . 'actions/notify-on-repeat-submissions.php';
+                    BREAK;
+            endswitch;
+        }
+
+        /**
+         * Enqueue FrontEnd Scripts
+         */
+        public static function enqueue_frontend_scripts() {
+            global $post;
+
+            /* Only load script on posts and pages or object pages */
+            if (!$post || !isset($post->ID) ) {
+                return;
+            }
+
+            /* ignore archive and author pages */
+            if (is_author() || is_archive() || is_search()) {
+                return;
+            }
+        }
+
+        /**
+         * Autopopulate hidden ninja form values
+         * @param  string $value          [description]
+         * @param  object $field_class    [description]
+         * @param  array $field_settings [description]
+         * @return string                 [description]
+         */
+        public static function autopopulate_hidden_values( $value , $field_class , $field_settings ) {
+
+            /* if field type is not hidden then skip */
+            if ($field_class != 'hidden') {
+                return $value;
+            }
+
+            /* if field does not contian job_id as a key then return */
+            if ( isset($field_settings['key']) && $field_settings['key'] == 'job_id') {
+
+                /* get job id from referring URL */
+                $referrer = wp_get_referer();
+                $parts = explode('job/' , $referrer);
+                $job_id = (isset($parts[1])) ? str_replace('/','',$parts[1]) : 'no job id detected';
+
+                return $job_id;
+            }
+
+
+            return $value;
+        }
+
+        /**
+         * Registers action to notify administrator on duplicate email address in job submission
+         * @param  [type] $actions [description]
+         * @return [type]          [description]
+         */
+		public static function register_actions($actions) {
+			$actions[ 'codeable-notify-administrator-on-repeat-submission' ] = new NF_Actions_Codeable_Notify_On_Repeat_Submission();
+
+			return $actions;
+		}
+
+
+
+    }
+
+    /* launch plugin */
+    add_action('plugins_loaded' , 'codeable_load_amberresources' , 1);
+	function codeable_load_amberresources() {
+        new Codeable_AmberResources;
+	}
+
+
+}
